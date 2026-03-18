6ac3c7758642590966dd3c0c9b7105daf1970681
Bavanetha M R <bavanethamr@gmail.com>
2026-03-18 11:27:36 +0530

    builded

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,39 +8,10 @@ import "./App.css";
 export default function App() {
   return (
     <Router>
-      {/* <div className="app-container min-h-screen flex flex-col">
-        {/* Navigation 
-        <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-          <h1 className="text-xl font-bold">Campus Navigator</h1>
-          <div className="space-x-4">
-            <Link
-              to="/"
-              className="flex items-center gap-1 hover:text-yellow-300"
-            >
-              <FaMap /> Map
-            </Link>
-            <Link
-              to="/admin"
-              className="flex items-center gap-1 hover:text-yellow-300"
-            >
-              <FaUserShield /> Admin
-            </Link>
-          </div>
-        </nav>
-
-        {/* Routes 
-        <main className="flex-1 p-4 bg-gray-100"> */}
           <Routes>
             <Route path="/" element={<CampusMapPage />} />
             <Route path="/admin" element={<AdminPanel />} />
           </Routes>
-        {/* </main>
-
-        {/* Footer 
-        <footer className="bg-blue-600 text-white p-2 text-center">
-          © 2026 Campus Navigator
-        </footer>
-      </div> */}
     </Router>
   );
 }
